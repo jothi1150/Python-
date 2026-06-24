b315610e7e6972673c1dde4fefa0b1c31f235b08
JOTHI S <jothi4667@gmail.com>
2026-06-24 14:26:24 +0530

    Add files via upload

---
+++ Lab1 task.py
@@ -0,0 +1,48 @@
+#Easy task
+#1Tempture converter
+'''c=int(input("Enter C:"))
+F=int(input("Enter F:"))
+print("converts it to Fahrenheit:",c*1.8+32)'''
+
+
+#2Simple interest Calculator
+'''P=int(input("Enter the principal amount:"))
+R=int(input("Enter the Annual Rate of interest:"))
+T=int(input("Enter the Time:"))
+print("Simple Interest:",P*R*T/100)'''
+
+
+#3Square and Cube Generator
+'''S=int(input("Enter n:"))
+print("Square Value:",S**2)
+print("Cube Value:",S**3)'''
+
+#4Total Minutes & Seconds
+'''T=float(input("Enter the hours:"))
+h=T*60
+s=h*60
+print(h)
+print(s)'''
+
+#5Average of Three Marks
+'''t=int(input("Enter Tamil mark:"))
+e=int(input("Enter English mark:"))
+m=int(input("Enter Maths mark:"))
+print("Final Average Score:",t+e+m/3)'''
+
+
+#hard task
+#6)Digits Extractor
+f=345
+a=f%10
+print("Last digit:",a)
+d=a//10
+print(d)
+
+#7)Swap Without a Third Variable using bitwise operators
+a=10
+b=20
+print("swap the values,a*b)
+
+
+
