24483a09cc4e5d76f5f4b4cf9399322f9f1ed8d6
JOTHI S <jothi4667@gmail.com>
2026-06-26 14:28:05 +0530

    Add files via upload

---
+++ day 2 lab task.py
@@ -0,0 +1,91 @@
+# 1)student pass or fail
+'''m=int(input("enter your mark:"))
+if m>=35:
+    print("pass")
+else:
+    print("fail")'''
+
+#2)check voting eligibility
+'''a=int(input("enter your age:"))
+if a>=18:
+      print("you are eligible to vote")
+else:
+    print("not eligible")'''
+
+    
+#3)calculate the customer final amount
+'''amount=5000
+d=10
+final=amount*d/100
+print("final amount:",final)'''
+
+#4)verify the password
+'''s_pass=1234
+p=int(input("enter your password:"))
+if p==s_pass:
+      print("you can access a website")
+else:
+    print("wrong password")'''
+
+#5)based on marks
+'''m=int(input("enter your marks:"))
+if m>=90:
+      print("A grade")
+elif m>=75:
+    print("B grade")
+elif m>=50 :
+    print("C grade")
+else:
+    print("fail")'''
+
+#6)Simple calculator
+'''a=int(input("enter your a:"))
+b=int(input("enter your b:"))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("addition:",add)
+print("subtraction:",sub)
+print("multiplilcation:",mul)
+print("Division:",div)'''
+
+#7)Calculate electricity charges
+'''u=int(input("enter you units:"))
+if u<=101:
+      print("₹2/unit")
+elif u<=200:
+      print("₹3/unit")
+elif u>200:
+    print("₹5/unit")
+else:
+    print("enter correct units:")'''
+
+#8)ticket fare based on age
+'''a=int(input("enter your age:"))
+if a<5:
+    print("Ticket free")
+elif a<12:
+    print("Ticket price 50")
+elif a>=12:
+    print("Ticket price 100")
+else:
+    print("enter your age correct")'''
+
+#9) bank approves a loan
+'''a=int(input("enter you amount:"))
+c=int(input("enter you credit score:"))
+if a>=30000 and c>=700:
+      print("Eligible for loan approved")
+else:
+    print(" Not Eligible")'''
+
+#10)student is eligible for college admission
+a=int(input("enter your academics score:"))
+e=int(input("enter hour entrance score:"))
+if a>60 and a>70:
+    print("Eligible for college admission")
+else:
+    print("not eligible")
+
+      
