ccd0fb600757e67f6a049a50e48da55c1fdb2b6d
JOTHI S <jothi4667@gmail.com>
2026-07-01 12:07:32 +0530

    Add files via upload

---
+++ day 3 lab task.py
@@ -0,0 +1,63 @@
+#task 1
+'''for i in range(5):
+    print("Jothi")'''
+    
+#task 2
+'''for i in range(1,21):
+    print(i)'''
+
+#task 3
+'''for i in range(20,0,-1):
+    print(i)'''
+
+#task 4
+'''for i in range(1,51):
+    if i%2==0:
+        print(i)'''
+
+#task 5
+'''for i in range(1,51):
+    if i%2!=0:
+        print(i)'''
+#task 6
+'''total=0
+for i in range(1,11):
+    total+=i
+    print(total)'''
+#task 7
+'''num=int(input("Enter a number:"))
+for i in range(1,11):
+        print(num,"x",i,"=",num*i)'''
+#task 8
+'''name=str(input("enter your name:"))
+count=0
+for letter in name:
+         count+=1
+         print(count)'''
+#task 9
+'''word=str(input("enter a word:"))
+for letter in word:
+    print(letter)'''
+#task 10
+'''word=str(input("enter a word:"))
+count=0
+for letter in word:
+    if letter in"aeiouAEIOU":
+        count+=1
+        print(count)'''
+#task 11
+'''n = 5
+
+for i in range(n):
+    for j in range(n):
+        if i==j or i+j==n-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()'''
+#task 12
+'''for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()'''
+         
