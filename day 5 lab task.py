7bc53ccfadb0b7e87e57c7949a4292f047ac2594
JOTHI S <jothi4667@gmail.com>
2026-07-10 14:22:55 +0530

    Add files via upload

---
+++ day 5 lab task.py
@@ -0,0 +1,67 @@
+#task 1
+'''username=input("Enter username:")
+print(username.upper())   
+print(username.lower()) 
+print(username.title())'''
+
+#task 2
+'''email=input("Enter email:")
+print("starts with letter:",email[0].isalpha())
+print("Ends with.com:",email.endswith(".com"))'''
+
+#task 3
+'''sentence = input("Enter a sentence: ")
+
+print("Count of 'a':", sentence.count('a'))
+print("Count of 'e':", sentence.count('e'))'''
+
+#task 4
+'''word = input("Enter a word: ")
+char = input("Enter a character to search: ")
+
+pos = word.find(char)
+if pos != -1:
+    print("Character found at position:", pos)
+else:
+    print("Character Not Found")'''
+
+#task 5
+    
+'''password = input("Enter password: ")
+
+print("Only alphabets:", password.isalpha())
+print("Only digits:", password.isdigit())
+print("Alphanumeric:", password.isalnum())'''
+
+#task 6
+'''name = input("Enter your name: ")
+
+print(name.center(20, '*'))
+print(name.ljust(20, '-'))
+print(name.rjust(20, '-'))'''
+
+#task 7
+'''text = input("Enter a sentence: ")
+
+print(text.upper()) 
+print(text.lower())       
+print(text.swapcase())'''
+
+#task 8
+'''sentence = input("Enter a sentence: ")
+
+print(sentence.replace("Python", "Java"))'''
+
+#task 9
+'''roll = input("Enter roll number: ")
+
+print(roll.zfill(10))'''
+#task 10
+'''word = input("Enter a word: ")
+
+print("All uppercase:", word.isupper())
+print("All lowercase:", word.islower())
+print("Title case:", word.istitle())'''
+
+
+
