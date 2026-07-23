aa91f1cf8c11ea8abd76dbd670d8d7b0c692ee04
JOTHI S <jothi4667@gmail.com>
2026-07-23 08:32:15 +0530

    Add files via upload

---
+++ day 1 tasks.py
@@ -0,0 +1,97 @@
+#Task 1
+'''a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Floor Division",a//b)
+print("Exponentiation",a**b)'''
+
+#Task 2
+#Rectangle
+'''l=float(input("Enter length of rectangle:"))
+b=float(input("Enter breadth of rectangle:"))
+print("Area of Rectangle:",l*b)
+print("perimeter of Rectangle:",2*(l+b))
+#square
+side=float(input("Enter side of square:"))
+print("Area of square:",side**2)
+print("perimeter of square:",4**side)
+#Circle
+r=float(input("Enter radius of circle:"))
+print("Area of Circle:",22/7*r**2)
+print("Circumference of Circle:",2*22/7*r)'''
+#Task 3
+'''a=int(input("Enter a number:"))
+b=int(input("Enter b number:"))
+c=int(input("Enter c number:"))
+print("Average:",(a+b+c)/3)'''
+
+#Task 4
+'''a=int(input("Enter a number:")
+b=int(input("Enter b number:")
+if a==b:
+      print("Both a and b are equal")
+elif a>b:
+    print("a is greater")
+else:
+    print(" a is less than or equal to b")'''
+#Task 5
+'''num=float(input("Enter a number:"))
+result=num**0.5
+print("Square root is:",result)'''
+
+#Task 6
+'''p=float(input("Enter the P value:"))
+r=float(input("Enter the R value:"))
+t=float(input("Enter the T value:"))
+si=(p*r*t)/100
+ci=p*((1+r/100)**t)-p
+print("Simple Interest:",si)
+print("Compound Interest:",ci)'''
+
+#Task 7
+'''x=10
+x+=5
+print("After x+=5:",x)
+x-=3
+print("After x-=3:",x)
+x*=2
+print("After x*=2:",x)
+x/=4
+print("After x/=4:",x)
+x%=2
+print("After x%=2",x)
+x**=3
+print("After x**=3:",x)'''
+
+#Task 8
+'''a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+print("Before Swapping")
+print("a=",a)
+print("b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("After Swapping")
+print("a=",a)
+print("b=",b)'''
+      
+#Task 9
+'''username="Jothi"
+password="123"
+u=input("Enter the username:")
+p=input("Enter the password:")
+if username==u and password==p:
+    print("you can access")
+else:
+    print("Invalid username or password")'''
+#Task 10
+num=float(input("Enter a number:"))
+result=num**(1/3)
+print("Cube Root:",result)
+
+            
