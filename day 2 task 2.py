ed3b4b3065d70437d4aca00fbc32569b6175e7c1
JOTHI S <jothi4667@gmail.com>
2026-07-24 09:29:05 +0530

    Add files via upload

---
+++ day 2 task 2.py
@@ -0,0 +1,35 @@
+#Task 1
+#Task 2
+#Task 3
+'''num=int(input("Enter the number between 1 and 100:"))
+for i in range(1,num+1):
+    if i%9==0:
+        break
+    if i%3==0:
+        continue
+    print(i)'''
+#Task 4
+#Task 5
+#Task 6
+'''num=int(input("Enter a number:"))
+while num>=1:
+    if num==5:
+        pass
+    print(num)
+    num-=1'''
+#Task 7
+'''word="PYTHON"
+for letter in word:
+    if letter=="H":
+        continue
+    print(letter)'''
+#Task 8
+num=20
+while num>=1:
+    if num==17:
+        num-=1
+        continue
+    if num==13:
+        break
+    print(num)
+    num-=1
