c63618349bc9ac53dba9e11472774192e484a92e
JOTHI S <jothi4667@gmail.com>
2026-07-24 09:27:55 +0530

    Add files via upload

---
+++ day 2 tasks1.py
@@ -0,0 +1,53 @@
+#Task 1
+'''for i in  range(1,11):
+    print(i)'''
+#Task 2
+'''num=int(input("Enter the Tables num :"))
+for i in range(1,11):
+    print(num ,"x",i,"=",num*i)'''
+#Task 3
+'''for i in range(1,51):
+    if i%2==0:
+        print(i)'''
+#Task 4
+'''num=int(input("Enter a number:"))
+fact=1
+for i in range(1,num+1):
+    fact*=i
+print("Factorial=",fact)'''
+#Task 5
+'''num=int(input("Enter a number:"))
+b=0
+t=num
+while t>0:
+    d=t%10
+    b=(b*10)+d
+    t=t//10
+print(b)
+if b==num:
+    print("palindrome")
+else:
+    print("Not a palindrome")'''
+#Task 6
+'''for i in range(1,5):
+    print("*"*i)'''
+#Task 7
+'''for i in range(2,21):
+ if i%2==0:
+     print(i)'''
+
+#Task 8
+'''num=1
+while num<=10:
+     print(num)
+     num+=1'''
+#Task 9
+'''num=1
+while num<=20:
+    print(num)
+    num+=1'''
+#Task 10
+'''for i in range(51,0,-1):
+    print(i)'''
+
+
